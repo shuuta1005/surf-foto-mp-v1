cd81a6d53a3d5b51693d2f48537ff98ddb6c5660
Shuta1005 <shutafute26@gmail.com>
2025-02-24 09:24:30 +1100

    Fixed typescript issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import HighlightedGalleries from "@/components/shared/highlightedGallery/HighlightedGalleries";
 import UploadPhoto from "@/components/admin/photoUpload";
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = async () => {
   await delay(1000);
